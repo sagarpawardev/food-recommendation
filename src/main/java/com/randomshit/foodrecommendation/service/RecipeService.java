48bd9c9d755c9a1ecb8ce2875de2cae8266f0c9d
sagarpawardev <sagar.pawar.dev@gmail.com>
2020-05-30 01:00:42 +0530

    revert previous commit

--- src/main/java/com/randomshit/foodrecommendation/service/RecipeService.java
+++ src/main/java/com/randomshit/foodrecommendation/service/RecipeService.java
@@ -1,53 +1,22 @@
 package com.randomshit.foodrecommendation.service;
 
 import com.randomshit.foodrecommendation.pojo.Recipe;
-import com.randomshit.foodrecommendation.pojo.RecipeList;
-import org.springframework.util.ResourceUtils;
-import org.yaml.snakeyaml.Yaml;
 
-import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.OptionalInt;
-import java.util.Random;
 
 public class RecipeService {
-
-    private List<Recipe> recipes = new ArrayList<>(0);
-    private Random random = new Random();
-
-    public RecipeService(){
-        loadRecipes();
-    }
-
-
     public List<Recipe> getRecipes(){
-        return recipes;
+        Recipe recipe = new Recipe();
+        recipe.setName("Omelette");
+        recipe.setUrl("https://www.youtube.com/watch?v=hViEZ9zz83E");
+
+        return List.of(recipe);
     }
 
     public Recipe getRecommendation(){
-        OptionalInt optInt = random.ints(0, recipes.size()).findFirst();
-        if(optInt.isPresent()){
-            int idx = optInt.getAsInt();
-            return recipes.get(idx);
-        }
-
         Recipe recipe = new Recipe();
-        recipe.setName("No Suggestion");
+        recipe.setName("Omelette");
+        recipe.setUrl("https://www.youtube.com/watch?v=hViEZ9zz83E");
         return recipe;
     }
-
-    //<editor-fold desc="Private Method">
-    private void loadRecipes(){
-        try {
-            File resource = ResourceUtils.getFile("classpath:recipes.yml");
-            Yaml yml = new Yaml();
-            RecipeList recipeList = yml.loadAs(new FileInputStream(resource), RecipeList.class);
-            recipes.addAll(recipeList.getRecipes());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    //</editor-fold>
 }
