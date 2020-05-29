2ff0518f4b5a712a52c3e574dffe70e8560fadc0
sagarpawardev <sagar.pawar.dev@gmail.com>
2020-05-30 01:25:56 +0530

    recommendation random logic

---
+++ src/main/java/com/randomshit/foodrecommendation/pojo/RecipeList.java
@@ -0,0 +1,15 @@
+package com.randomshit.foodrecommendation.pojo;
+
+import java.util.List;
+
+public class RecipeList {
+    private List<Recipe> recipes;
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(List<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
